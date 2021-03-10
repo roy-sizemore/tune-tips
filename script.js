8da0a538c157c53c6725c2046daccb0c87cdff21
Roy Sizemore <rsizemore@swampcastle.home>
2021-03-10 14:13:58 -0500

    Updated getArtist function with bottom portion of the page content

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 $(document).ready(() => {
-    
     // Add divs and Bootstrap classes for Jumbotron, containers, search input, artist and the button and append to the body
     const $jumboDiv = $('<div>').addClass('jumbotron jumbotron-fluid');
     const $contain1 = $('<div>').addClass('container container-fluid d-flex flex-row pl-5');
-    const $h1 = $('<h1>').addClass('pb-3 pl-5').text('Welcome to TuneTips!');
-    const $btn = $('<button>').addClass('btn btn-dark flex-shrink-0').text('Tune Tips!');
-    const $searchInput = $('<input>').attr({type: 'text', placeholder: 'Enter Artist'}).addClass('form-control aria-label text');
-    const $artist = $('<div>').addClass('h2 pt-3 pb-2');
+    const $h1 = $('<h1>').attr('id', 'h1').addClass('pb-3 pl-5').text('Welcome to Tune Tips!');
+    const $btn = $('<button>').addClass('btn btn-dark flex-shrink-0 col-2').text('Search');
+    const $searchInput = $('<input>').attr({type: 'text', placeholder: 'Enter Artist, ex: The Weekend'}).addClass('form-control aria-label text');
     $contain1.append($searchInput, $btn);
     $jumboDiv.append($h1, $contain1);
     $('body').append($jumboDiv);
 
-    const showArtist = () => {
-        $artist.addClass('text-capitalize').text($searchInput.val());
-        $('body').append($artist);
-    };
-
+    // Retrieve artist info and top 10 tracks 
     const getArtist = () => {
+        const $topTracks = $('<h2>').attr('id', 'h2').text(`Top 10 Tracks for:\n${$searchInput.val()}`);
+        const $artistInfo = $('<h2>').attr('id', 'h2').text(`Info about:\n${$searchInput.val()}`);
+        const $contain2 = $('<div>').addClass('container container-fluid d-flex flex-row p-5');
+        const $trackDiv = $('<div>').addClass('container-s col-4 p-5');
+        const $infoDiv = $('<div>').addClass('container-s col-4 p-5');
+        $infoDiv.append($artistInfo);
+        $trackDiv.append($topTracks);
+        $contain2.append($infoDiv, $trackDiv);
+        $('body').append($contain2);
+
         // AudioDB API
         $.ajax({
             // Retrieves top 10 tracks
@@ -37,7 +41,7 @@ $(document).ready(() => {
                 "x-rapidapi-host": "artist-info.p.rapidapi.com"
             },
             success: (data) => {
-                console.log(data)
+                console.log(JSON.stringify(data));
             }
         });
     };
