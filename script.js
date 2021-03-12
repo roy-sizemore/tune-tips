f3a8cf2503ee32953a2cb23db7e828301e6bec1d
Eric Shaw <ericshawmail@gmail.com>
2021-03-11 21:22:43 -0500

    Updated css, javascript

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
     // Add divs and Bootstrap classes for Jumbotron, containers, search input, artist and the button and append to the body
     const $jumboDiv = $('<div>').addClass('jumbotron jumbotron-fluid newJumbo'); // cut/paste out classes and/or ids you don't want to use and put them in comments off to the side
-    const $contain1 = $('<div>').addClass('container container-fluid d-flex flex-row pl-5');
+    const $contain1 = $('<div>').addClass('container-fluid d-flex align-items-center');
     const $h1 = $('<h1>').attr('id', 'h1').addClass('pb-3 pl-5').text('Welcome to Tune Tips!');
     const $btn = $('<button>').addClass('btn btn-dark flex-shrink-0 col-2').text('Search');
     const $searchInput = $('<input>').attr({type: 'text', placeholder: 'Enter Artist, ex: The Weekend'}).addClass('form-control aria-label text');
@@ -14,10 +14,10 @@ $(document).ready(() => {
         // Adds divs and Bootstrap to container/cards showing the artist's info and top tracks once the user inputs an artist
         const $topTracks = $('<h2>').addClass('text-capitalize search-input').text(`Top Tracks for: ${$searchInput.val()}`);
         const $artistInfo = $('<h2>').addClass('text-capitalize search-input').text(`Info about: ${$searchInput.val()}`);
-        const $contain2 = $('<div>').addClass('container container-fluid d-flex flex-column align-items-center');
-        const $trackDiv = $('<div>').addClass('container-s col-6 flex-column');
+        const $contain2 = $('<div>').addClass('container-fluid d-flex align-items-center');
+        const $trackDiv = $('<div>').addClass('container-s');
         const $olTrack = $('<ol>');
-        const $infoDiv = $('<div>').addClass('container-s col-6 ');
+        const $infoDiv = $('<div>').addClass('container-s');
         $infoDiv.append($artistInfo);
         $trackDiv.append($topTracks, $olTrack);
         $contain2.append($infoDiv, $trackDiv);
