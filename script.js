5e11684214c21c8eb7542e2d5e3ff977cdc800c2
Roy Sizemore <rsizemore@swampcastle.home>
2021-03-12 16:01:51 -0500

    Changed form input field example to MJ. Provided more detail in script.js comments

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(() => {
-    // Add divs and Bootstrap classes for Jumbotron, containers, search input, artist and the button and append to the body
-    const $jumboDiv = $('<div>').addClass('jumbotron jumbotron-fluid newJumbo'); // cut/paste out classes and/or ids you don't want to use and put them in comments off to the side
+    // Add divs, Bootstrap, fontawesom search icon and custom CSS for Jumbotron, containers, search input and the button and append to the body
+    const $jumboDiv = $('<div>').addClass('jumbotron jumbotron-fluid newJumbo');
     const $contain1 = $('<div>').addClass('container-fluid d-flex align-items-center');
     const $h1 = $('<h1>').attr('id', 'h1').addClass('pb-3 text-center').text('Welcome to Tune Tips!');
     const $btn = $('<button>').addClass('btn btn-dark flex-shrink-0 col-2');
     const $btnIcon = $('<i>').addClass('fas fa-search');
-    const $searchInput = $('<input>').attr({type: 'text', placeholder: 'Enter Artist, ex: The Weekend'}).addClass('form-control aria-label text');
+    const $searchInput = $('<input>').attr({type: 'text', placeholder: 'Enter Artist, ex: Michael Jackson'}).addClass('form-control aria-label text');
     $btn.append($btnIcon);
     $contain1.append($searchInput, $btn);
     $jumboDiv.append($h1, $contain1);
@@ -16,7 +16,7 @@ $(document).ready(() => {
         // Leave function if $searchInput.val() is empty, ex: user just presses the Enter key or clicks Search
         if (!$searchInput.val()) return;
 
-        // Adds divs and Bootstrap to container/cards showing the artist's info and top tracks once the user inputs an artist
+        // Adds divs, Bootstrap and custom CSS to container/cards showing the artist's info and top tracks once the user inputs an artist
         const $topTracks = $('<h2>').addClass('text-capitalize search-input text-center pb-2').text(`Top Tracks for: ${$searchInput.val()}`);
         const $artistInfo = $('<h2>').addClass('text-capitalize search-input text-center pb-2').text(`Info about: ${$searchInput.val()}`);
         const $contain2 = $('<div>').attr('id', 'cont-2').addClass('container-fluid d-flex align-items-center');
@@ -30,19 +30,19 @@ $(document).ready(() => {
 
         // AudioDB API
         $.ajax({
-            // Retrieves top 10 tracks
+            // Retrieves top 10 tracks, creates li for each track and appends to $olTrack
             url: `https://theaudiodb.com/api/v1/json/523532/track-top10.php?s=${$searchInput.val()}`,
             method: 'GET',
             success: (data) => {
-                    for (i = 0; i < data.track.length; i++) {
-                        $olTrack.append($('<li>').text(data.track[i].strTrack));
-                    };
-            },
+                for (i = 0; i < data.track.length; i++) {
+                    $olTrack.append($('<li>').text(data.track[i].strTrack));
+                };
+            }
         });
 
         // last.fm API
         $.ajax({
-            // Retrieves artist information
+            // Retrieves artist information and appends to $infoDiv
             url: `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${$searchInput.val()}&api_key=c8fa358f2006de3b95aa7bf6026a6017&format=json`,
             method: "GET",
             success: (response) => {
@@ -50,11 +50,11 @@ $(document).ready(() => {
             }
         });
 
-        // Clear form input field
+        // Clears form input field
         $searchInput.val('');
     };
 
-    // Button and Enter key functionality
+    // on click Button and Enter key functionality to run getArtist function
     $btn.on('click', getArtist);
     $searchInput.on('keypress', (enter) => {
         if (enter.which === 13) {
@@ -62,12 +62,4 @@ $(document).ready(() => {
         };
     });
 });
-
-
-/* How to add an id to a jQuery statement:
-const sampleStatement = $('<div>').attr('id', 'id-name').addClass('first-class second-class');
-
-If the variable has already been declared:
-sampleStatement.attr('id', 'first-id second-id').addClass('first-class second-class);
-*/
 
