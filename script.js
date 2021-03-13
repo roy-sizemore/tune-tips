6ca39544d2ceb16d764e3c03c47823deeb28c9fb
Roy Sizemore <rsizemore@swampcastle.home>
2021-03-12 21:09:45 -0500

    Fixed searchHist on-click bugs

--- script.js
+++ script.js
@@ -20,15 +20,16 @@ $jumboDiv.append($h1, $contain1, $searchRow);
 $('body').append($jumboDiv);
 
 // Retrieve artist info and top 10 tracks. Adds a container div and two divs, one for the artist bio and one for the top tracks. Top tracks are displayed in an ordered list. divs are then appended to the body
-const getArtist = () => {
+const getArtist = (artistName) => {
     $olTrack.text(" ");
     $infoDiv.text(" ");
     // Returns out of function if $searchInput.val() is empty, ex: user just presses the Enter key or clicks Search
     if (!$searchInput.val()) return;
+    console.log(artistName);
 
     // Adds divs, Bootstrap and custom CSS to container/cards showing the artist's info and top tracks once the user inputs an artist
-    $topTracks.addClass('text-capitalize search-input text-center pb-2').text(`Top Tracks for: ${$searchInput.val()}`);
-    $artistInfo.addClass('text-capitalize search-input text-center pb-2').text(`Info about: ${$searchInput.val()}`);
+    $topTracks.addClass('text-capitalize search-input text-center pb-2').text(`Top Tracks for: ${artistName ? artistName : $searchInput.val()}`);
+    $artistInfo.addClass('text-capitalize search-input text-center pb-2').text(`Info about: ${artistName ? artistName : $searchInput.val()}`);
     
     $jumboDiv.append($searchRow);
     $infoDiv.append($artistInfo);
@@ -61,13 +62,11 @@ const getArtist = () => {
     }).then((response) => {
         if (!(response.artist.name in localStorage)) {
             localStorage.setItem(response.artist.name, response.artist.name);
-            $searchRow.append($('<button>').addClass('btn btn-light searchHist').text(response.artist.name));
-            $('.searchHist').on('click', () => {
+            $searchRow.append($('<button>').addClass('btn btn-light searchHist').text(response.artist.name).on('click', () => {
                 $searchInput.val(response.artist.name);
-                getArtist();
+                getArtist(response.artist.name);
                 $searchInput.val('');
-            });
-            $searchInput.val('');
+            }));
         };
     });
 
